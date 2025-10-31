99ae1fb1b44c3533f2bc2113e6039f015a1e4efc
Ryan Tusi <ryantusi45@gmail.com>
2025-10-31 08:31:01 +0530

    config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,13 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "./",
   plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://live-vessel-tracker-backend-express.onrender.com",
         changeOrigin: true,
       },
     },
